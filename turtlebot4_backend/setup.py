3dd8ba4b536c3b18c48e29cc849a63b41953a6e1
uswup <uswup@student.kit.edu>
2025-12-24 18:29:26 +0100

    changed structure of project

---
+++ turtlebot4_backend/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+package_name = 'turtlebot4_backend'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Saadhvi',
+    maintainer_email='uswup@student.kit.edu',
+    description='Backend package for TurtleBot4',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            # Example: 'node = turtlebot4_backend.turtlebot4_controller:main'
+        ],
+    },
+)
