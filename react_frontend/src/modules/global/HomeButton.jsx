4f69d5492971213fadf5e40e7cb87329fb67b9a7
uykve <uykve@student.kit.edu>
2026-01-19 16:04:47 +0100

    Added home button and fixed index.cc (cleaner, more global variables)

--- react_frontend/src/modules/global/HomeButton.jsx
+++ react_frontend/src/modules/global/HomeButton.jsx
@@ -1,11 +1,12 @@
 import './HomeButton.css';
+import homeIcon from './assets/homeIcon.svg'
 import { Link } from 'react-router-dom';
 
 export default function HomeButton() {
     return ( 
         <Link to="/" className="home-button" aria-label="Home">
-            <svg 
-                src='./assets/homeIcon.svg' 
+            <img 
+                src={homeIcon} 
                 alt="Home Icon" 
                 className="home-icon"
             />
